ecc93074f76ef95eaa7443825962e169a96f2413
songhuaming <songhuaming@navinfo.com>
2019-03-22 13:34:42 +0800

    增加notify提示

--- app/src/main/java/com/fundrive/navaidlclient/MainActivity.java
+++ app/src/main/java/com/fundrive/navaidlclient/MainActivity.java
@@ -143,6 +143,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         public void onNotify(int ia_cmd, String ia_json) throws RemoteException {
             System.out.println(ia_cmd);
             System.out.println("" + ia_json);
+            Toast.makeText(MainActivity.this,"cmd = "+ia_cmd +"\n json = "+ia_json,Toast.LENGTH_LONG).show();
         }
 
         @Override
